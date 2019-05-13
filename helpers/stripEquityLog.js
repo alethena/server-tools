b8b91eee66889fcdcc7acbfbca570845041ad951
rickenbb <b.rickenbacher@intergga.ch>
2019-05-13 12:16:26 +0200

    Small fix

--- helpers/stripEquityLog.js
+++ helpers/stripEquityLog.js
@@ -5,21 +5,40 @@ async function stripLog(logEntry, company) {
         try {
             const timestamp = await blockNumberToTimestamp(logEntry.blockNumber);
             // console.log(logEntry);
-            resolve([
-                logEntry.transactionHash,
-                logEntry.event,
-                company.equityAddress,
-                timestamp,
-                logEntry.blockNumber,
-                logEntry.transactionIndex,
-                logEntry.logIndex,
-                logEntry.returnValues.from,
-                logEntry.returnValues.to,
-                Number(logEntry.returnValues.value.toString()),
-                logEntry.returnValues.shareholder,
-                logEntry.returnValues.amout,
-                logEntry.returnValues.message
-            ]);
+            if (logEntry.event === "Transfer") {
+                resolve([
+                    logEntry.transactionHash,
+                    logEntry.event,
+                    company.equityAddress,
+                    timestamp,
+                    logEntry.blockNumber,
+                    logEntry.transactionIndex,
+                    logEntry.logIndex,
+                    logEntry.returnValues.from,
+                    logEntry.returnValues.to,
+                    Number(logEntry.returnValues.value.toString()),
+                    logEntry.returnValues.shareholder,
+                    logEntry.returnValues.amout,
+                    logEntry.returnValues.message
+                ]);
+            } else {
+                resolve([
+                    logEntry.transactionHash,
+                    logEntry.event,
+                    company.equityAddress,
+                    timestamp,
+                    logEntry.blockNumber,
+                    logEntry.transactionIndex,
+                    logEntry.logIndex,
+                    logEntry.returnValues.from,
+                    logEntry.returnValues.to,
+                    logEntry.returnValues.value,
+                    logEntry.returnValues.shareholder,
+                    logEntry.returnValues.amout,
+                    logEntry.returnValues.message
+                ]);
+            }
+
         } catch (error) {
             reject(error);
         }
