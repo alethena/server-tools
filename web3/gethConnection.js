c41d8d3e593148870edb499600a74519e4fa80bc
rickenbb <b.rickenbacher@intergga.ch>
2019-12-02 23:02:48 +0100

    Current state on Luzi node

--- web3/gethConnection.js
+++ web3/gethConnection.js
@@ -3,7 +3,7 @@ const net = require('net');
 const config = require('./web3Config.json');
 var web3;
 var Raven = require('raven');
-Raven.config('https://853db40d557b42189a6b178ba7428001@sentry.io/1470742').install();
+Raven.config('https://c109b2efca9941dcac153716d23172d6@sentry.io/1811490').install();
 
 try {
     web3 = new Web3(
